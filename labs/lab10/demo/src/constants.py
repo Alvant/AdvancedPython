ed5145e12ff4be92330059766e061b2417f9faff
Vasiliy <alvasian@yandex.ru>
2020-11-11 01:04:07 +0300

    add example for lab 10

---
+++ labs/lab10/demo/src/constants.py
@@ -0,0 +1,9 @@
+IO_SITES = [
+    "https://www.jython.org",
+    "https://en.wikipedia.org/wiki/Ant",
+] * 80
+
+_CPU_BIG_NUMBER = 5_000_000
+_CPU_NUM_TIMES = 20
+
+CPU_BIG_NUMBERS = [_CPU_BIG_NUMBER + x for x in range(_CPU_NUM_TIMES)]
---
+++ labs/lab10/demo/src/cpu_asyncio.py
@@ -0,0 +1,33 @@
+"""A useless example of applying asyncio to a CPU bound problem
+"""
+import asyncio
+import time
+
+from typing import Awaitable, List
+
+from constants import CPU_BIG_NUMBERS
+from utils import show_execution_time
+
+
+async def cpu_bound(number: int) -> int:
+    return sum(i * i for i in range(number))
+
+
+async def find_sums(numbers: List[int]) -> Awaitable[None]:
+    tasks = list()
+    
+    for number in numbers:
+        task = asyncio.ensure_future(cpu_bound(number))
+        tasks.append(task)
+     
+    await asyncio.gather(*tasks, return_exceptions=True)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+
+    show_execution_time(
+        func=lambda: loop.run_until_complete(find_sums(CPU_BIG_NUMBERS)),
+    )
+
+    loop.close()
